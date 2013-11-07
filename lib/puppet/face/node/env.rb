e89a4bada50d3715cd5ee8734d5c19b610a0c42f
Zack Smith <zack@puppetlabs.com>
2013-11-07 01:14:49 +0000

    Hack someoutput color
    Prior to this commit the headers were not clear what lines was what

--- lib/puppet/face/node/env.rb
+++ lib/puppet/face/node/env.rb
@@ -22,39 +22,45 @@ Puppet::Face.define(:node, '0.0.1') do
 
     when_invoked do |node_name, options|
       Puppet[:clientyamldir] = Puppet[:yamldir]
+      output = [ {'Name' => 'Environment'} ]
       if node_name == '*'
        Puppet::Node.indirection.terminus_class = :yaml
         unless nodes = Puppet::Node.indirection.search(node_name)
           raise "Nothing returned from yaml terminus (yamldir set?)"
         end
-        output = nodes.map { |node| Hash[node.name => node.environment] }
-        output
+        output << nodes.map { |node| Hash[node.name => node.environment] }
+        output.flatten
       else
         unless node = Puppet::Node.indirection.find(node_name,:terminus => 'yaml' )
           raise "Nothing returned from yaml terminus for (#{node_name})"
         end
         clientcert = node.parameters['clientcert']
         raise "Results returned (#{clientcert})" if clientcert != node_name
-        output = []
         output << Hash[ node.name => node.environment]
         output
       end
     end
 
     when_rendering :console do |output|
+      padding = '  '
+      headers = {
+        'node_name'   => 'Name',
+        'environment' => 'Environment',
+      }
+
+      min_widths = Hash[ *headers.map { |k,v| [k, v.length] }.flatten ]
+      min_widths['node_name'] = min_widths['environment'] = 80
+
+      min_width = min_widths.inject(0) { |sum,pair| sum += pair.last } + (padding.length * (headers.length - 1))
+
+      terminal_width = [Puppet::Util::Terminal.width, min_width].max
+
+      highlight = proc do |color,s|
+        c = colorize(color, s)
+        c
+      end
+      n = 0
       output.collect do |results|
-        padding = '  '
-        headers = {
-          'node_name'   => 'Name',
-          'environment' => 'Environment',
-        }
-
-        min_widths = Hash[ *headers.map { |k,v| [k, v.length] }.flatten ]
-        min_widths['node_name'] = min_widths['environment'] = 80
-
-        min_width = min_widths.inject(0) { |sum,pair| sum += pair.last } + (padding.length * (headers.length - 1))
-
-        terminal_width = [Puppet::Util::Terminal.width, min_width].max
 
         columns = results.inject(min_widths) do |node_name,environment|
           {
@@ -68,9 +74,13 @@ Puppet::Face.define(:node, '0.0.1') do
         format = %w{node_name environment}.map do |k|
           "%-#{ [ columns[k], min_widths[k] ].max }s"
         end.join(padding)
-
         results.map do |node_name,environment|
-          format % [ node_name, environment ]
+          n += 1
+          if n.odd?
+            highlight[ :hwhite,format % [ node_name, environment ] ]
+          else
+            highlight[ :white,format % [ node_name, environment ] ]
+          end
         end.join
       end
     end
